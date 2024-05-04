a7eae427648f2ce17f648fae34b833ea3152600f
GoneSahlin <zach@sahlins.net>
2024-05-03 17:26:11 -0700

    generate_possible_patterns working

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,9 +5,14 @@ import numpy as np
 from utils import utils
 
 
+def load_test_df():
+  df = pd.read_csv("data/NRDY.csv")
+
+  return df
+
 def test_clean_df():
   # load and clean data
-  df = pd.read_csv("data/NRDY.csv")
+  df = load_test_df()
   df = utils.clean_df(df)
 
   # test percent_chg
@@ -20,10 +25,26 @@ def test_clean_df():
   assert type(df["Date"].iloc[0]) == date
   
 
+def test_generate_possible_patterns():
+  # load and clean data
+  df = load_test_df()
+  df = utils.clean_df(df)
+
+  possible_patterns = utils.generate_possible_patterns(df)
+
+  # test output
+  correct_first_pattern = [date.fromisoformat(x) for x in ["2023-05-03", "2023-08-03", "2023-11-03", "2024-02-05"]]
+  assert possible_patterns[0] == correct_first_pattern
+
+  correct_last_pattern = [date.fromisoformat("2024-05-02")]
+  assert possible_patterns[-1] == correct_last_pattern
+
+  assert len(possible_patterns) == 252
+
 
 def test_find_pattern():
   # load and clean data
-  df = pd.read_csv("data/NRDY.csv")
+  df = load_test_df()
   df = utils.clean_df(df)
 
   # find pattern
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import date
+from datetime import timedelta
 
 
 def clean_df(df: pd.DataFrame):
@@ -17,6 +18,41 @@ def clean_df(df: pd.DataFrame):
   return df
 
 
+def generate_possible_patterns(df: pd.DataFrame):
+  """Generates a list of all possible patterns."""
+  possible_patterns = []
+
+  max_date = df['Date'].iloc[-1]
+
+  dates_set = set(df['Date'])
+
+  for index, row in df.iterrows():
+    first_date = row['Date']
+
+    i = 0  # first_date should be the first date in the pattern
+    pattern = []
+    cur_date = first_date
+
+    # loop while cur_date is still within dataset, building a pattern of dates one quarter apart
+    while cur_date <= max_date:
+      # find closest date in df
+      while cur_date not in dates_set and cur_date <= max_date:
+        cur_date = (cur_date + pd.DateOffset(days=1)).date()
+
+      # check cur_date has not left range
+      if cur_date <= max_date:
+        pattern.append(cur_date)
+      
+      # create new date i quarters from first date, format as datetime.date
+      i += 1
+      cur_date = (first_date + pd.DateOffset(months=(3 * i))).date()
+
+    # append created pattern to possible patterns
+    possible_patterns.append(pattern)
+
+  return possible_patterns
+
+
 def find_pattern(df: pd.DataFrame):
   """
   Find patterns in stock prices.
@@ -27,9 +63,6 @@ def find_pattern(df: pd.DataFrame):
   Returns:
     pattern(list): dates with reoccurring changes    
   """
-  for row in df.iterrows():
-    pass
-
 
   pattern = []
 
